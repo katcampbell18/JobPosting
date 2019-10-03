3ea504720b1a6cf7f3f97b69fd319d8cc98aba03
katcampbell18 <ktrncampbell@gmail.com>
2019-10-03 16:53:48 -0400

    initial

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,60 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.*;
+
+
+
+@Controller
+public class HomeController {
+    @Autowired
+    JobRepository jobRepository;
+
+    @RequestMapping("/")
+    public String listJobs(Model model){
+        model.addAttribute("jobs", jobRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/add")
+    public String jobForm(Model model){
+        model.addAttribute("job", new Job());
+        return "jobform";
+    }
+
+    // process Search Bar form
+    @PostMapping("/processsearch")
+    public String searchResult(Model model, @RequestParam(name="search") String search){
+        model.addAttribute("jobs", jobRepository.findByTitleContainingIgnoreCase(search));
+        return "searchlist";
+    }
+
+    @PostMapping("/process")
+    public String processForm(@ModelAttribute Job job){
+        jobRepository.save(job);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showJob(@PathVariable("id") long id, Model model){
+        model.addAttribute("job", jobRepository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateJob(@PathVariable("id") long id, Model model){
+        model.addAttribute("job", jobRepository.findById(id).get());
+        return "jobform";
+    }
+
+    @RequestMapping("delete/{id}")
+    public String delJob(@PathVariable("id") long id){
+        jobRepository.deleteById(id);
+        return "redirect:/";
+    }
+}
+
+
